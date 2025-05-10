33fd82fc0194b6c6179b696eaa3f0f4d770e7583
marymargovich <marymargovich@gmail.com>
2025-05-10 15:35:08 +0300

    hw upload

--- lesson5_Homework/src/homework_loops.java
+++ lesson5_Homework/src/homework_loops.java
@@ -1,17 +1,23 @@
 public class homework_loops {
     public static void main(String[] args) {
-        System.out.println("_________TASK 1_________");
-        int res =countDigits(122333);
+        System.out.println("_________TASK 1_ WHILE________");
+        int res =countDigits(13);
         System.out.println("Count digits = " + res);
         System.out.println();
 
-//        res = countDigits2(14566);
-//        System.out.println("Count digits = " + res);
-//        System.out.println();
+        System.out.println("_________TASK 1_ FOR________");
+        res = countDigits2(14566);
+        System.out.println("Count digits = " + res);
+        System.out.println();
 
-        System.out.println("____________TASK 2__________");
-        luckyNumber(173854);
-
+        System.out.println("____________TASK 2_WHILE_________");
+        luckyNumber(173867);
+        System.out.println("____________TASK 2_FOR_________");
+        luckyNumber2(132231);
+        System.out.println("____________TASK 2_for2_________");
+        luckyNumber2(173867);
+        System.out.println("____________TASK 2_for3_________");
+        luckyNumber2(132231);
 
     }
 
@@ -21,13 +27,18 @@ public class homework_loops {
             return 1;
         }
         while(i !=0){
-            int temp = i % 10;
             counter+=1;
              i = i/10;
         }
         return counter;
     }
-
+    private static int countDigits2(int i) {
+        int counter;
+        for (counter=0; i !=0 ; counter++) {
+            i = i/10;
+        }
+        return counter;
+    }
 
     private static void luckyNumber(int num){
         int sum1 = 0;
@@ -43,11 +54,37 @@ public class homework_loops {
         else
             System.out.println("Unlucky ticket =(");
 
+    }
+    private static void luckyNumber2(int num) {
+        int sum1;
+        int sum2;
+        for (sum1 = 0, sum2 = 0; num != 0;){
+        sum1 += num % 10;
+        num = num / 10;
+        sum2 += num % 10;
+        num = num / 10;
+        }
+        if (sum1 == sum2)
+            System.out.println("Lucky ticket");
+        else
+                System.out.println("Unlucky ticket =(");
 
     }
+    private static void luckyNumber3(int num) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for (; num != 0; num = num / 100) {
+            sum1 += num % 10;
+            sum2 += (num / 10) % 10;
+        }
 
-
+        if (sum1 == sum2)
+            System.out.println("Lucky ticket");
+        else
+            System.out.println("Unlucky ticket =(");
+    }
 }
+
 /*
 1)public static void main(String[] args) {
 int res = countDigits( num: 123);
